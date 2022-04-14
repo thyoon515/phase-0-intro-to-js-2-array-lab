c2cdd8b1446bd583b859ef21277dd53408506c7b
thyoon515 <thyoon515@gmail.com>
2022-04-13 21:43:21 -0400

    commit tree 56af6455b9b226062ee1669fdbea3d0f8d8f2e77

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push("Milo", "Otis", "Garfield");
+});
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+    return cats;
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+
+function appendCat() {
+    const newCats = [...cats, "Broom"];
+    return newCats;
+}
+
+function prependCat() {
+    const newCats = ["Arnold", ...cats];
+    return newCats;
+}
+
+function removeLastCat() {
+    const newCats = cats.slice(0, cats.length -1);
+    return newCats;
+}
+
+function removeFirstCat() {
+    const newCats = cats.slice(1);
+    return newCats;
+}
